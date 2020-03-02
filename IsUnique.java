40afccb8151ad4ff63b90c97ece9469be83e1b74
Susan Driskill <56049351+susandriskill@users.noreply.github.com>
2020-03-01 23:03:08 -0800

    Add files via upload

--- IsUnique.java
+++ IsUnique.java
@@ -13,39 +13,38 @@
 public class IsUnique {
 	
 	public static void main(String[] args) {
-		int[] list1 = {3, 8, 12, 2, 9, 17, 43, -8, 46, 203, 14, 97, 10, 4};
-		int[] list2 = {4, 7, 2, 3, 9, 12, -47, -19, 308, 3, 74};
+		int[] array1 = {3, 8, 12, 2, 9, 17, 43, -8, 46, 203, 14, 97, 10, 4};
+		int[] array2 = {4, 7, 2, 3, 9, 12, -47, -19, 308, 3, 74};
 		//Testing
-		System.out.println(isUnique(list1));
-		System.out.println(isUnique(list2));
+		System.out.println(isUnique(array1));
+		System.out.println(isUnique(array2));
 	}
 	
-	public static boolean isUnique(int[] list) {
+	public static boolean isUnique(int[] array) {
 		//isUnique is to return true unless the proper conditions are met (if 
 		//there is a pair of values in the array that is passed as a parameter 
 		//that are equal)
 		boolean isUnique = true;
 		//Declare and initialize the variable int i = 0 to start the loop at 0
 		//(for a zero-indexed array)
-	    //Use i < list.length - 1 to iterate the loop the number of times needed
+		//Use i < array.length - 1 to iterate the loop the number of times needed
 		//to traverse the array
-	    //Use i++ to increment by 1 for each iteration of the loop
-	    for (int i = 0; i <= list.length - 1; i++) {
+		//Use i++ to increment by 1 for each iteration of the loop
+	    for (int i = 0; i <= array.length - 1; i++) {
 	        //Declare and initialize the variable int j = 0 to start the loop at 
 	    	//0 (for a zero-indexed array)
 	        //Use j < i to iterate the loop the number of times needed to traverse
 	        //the for loop
 	        //Use j++ to increment by 1 for each iteration of the loop
 	        for (int j = 0; j < i; j++) {
-	            //If any element in list j equals any element in list i, then 
+	            //If any element in array j equals any element in array i, then 
 	            //isUnique is to return false
-	            if (list[j] == list[i]) {
+	            if (array[j] == array[i]) {
 	                isUnique = false; 
 	            }
 	        }
 	    }
-
 	    return isUnique;
-	    }
-	
+    }
+
 }
