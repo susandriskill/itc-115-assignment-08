e7685476139fadfae8caaf2fbd3ac8afbb49ac76
Susan Driskill <56049351+susandriskill@users.noreply.github.com>
2020-03-02 08:09:59 -0800

    Adds IsSorted.java

--- IsSorted.java
+++ IsSorted.java
@@ -10,28 +10,27 @@
 public class IsSorted {
 
 	public static void main(String[] args) {
-		double[] list1 = {16.1, 12.3, 22.2, 14.4};
-		double[] list2 = {1.5, 4.3, 7.0, 19.5, 25.1, 46.2};
+		double[] arr1 = {16.1, 12.3, 22.2, 14.4};
+		double[] arr2 = {1.5, 4.3, 7.0, 19.5, 25.1, 46.2};
 		//Testing
-		System.out.println(isSorted(list1));
-		System.out.println(isSorted(list2));
+		System.out.println(isSorted(arr1));
+		System.out.println(isSorted(arr2));
 	}
 
-	public static boolean isSorted(double[] list) {
+	public static boolean isSorted(double[] array) {
 		//isSorted is to return true unless the proper conditions are met (the
-		//array is not sorted (nondecreasing) order
+		//array is not in sorted (nondecreasing) order
 		boolean isSorted = true;
 		//Declare and initialize the variable int i = 0 to start the loop at 0
 		//(for a zero-indexed array)
-	    //Use i < list.length - 1 to iterate the loop the number of times needed
+		//Use i < list.length - 1 to iterate the loop the number of times needed
 		//to traverse the array
-	    //Use i++ to increment by 1 for each iteration of the loop
-		for (int i = 0; i < list.length - 1; i++) {
-			if (list[i] > list[i + 1]) {
+		//Use i++ to increment by 1 for each iteration of the loop
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1]) {
 				isSorted = false;
 			}
 		}
-		
 		return isSorted;
 	}
 
